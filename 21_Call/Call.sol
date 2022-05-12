68b420775366e5bbe64580bdfc1bf643c19e321b
0xAA <d0u.gie@hotmail.com>
2022-05-12 20:47:05 +0800

    Create Call.sol

---
+++ 21_Call/Call.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract OtherContract {
+    uint256 private _x = 0; // 状态变量x
+    // 收到eth事件，记录amount和gas
+    event Log(uint amount, uint gas);
+
+    fallback() external payable{}
+
+    // 返回合约ETH余额
+    function getBalance() view public returns(uint) {
+        return address(this).balance;
+    }
+
+    // 可以调整状态变量_x的函数，并且可以往合约转ETH (payable)
+    function setX(uint256 x) external payable{
+        _x = x;
+        // 如果转入ETH，则释放Log事件
+        if(msg.value > 0){
+            emit Log(msg.value, gasleft());
+        }
+    }
+
+    // 读取x
+    function getX() external view returns(uint x){
+        x = _x;
+    }
+}
+
+contract Call{
+    // 定义Response事件，输出call返回的结果success和data
+    event Response(bool success, bytes data);
+
+    function callSetX(address payable _addr, uint256 x) public payable {
+        // call setX()，同时可以发送ETH
+        (bool success, bytes memory data) = _addr.call{value: msg.value}(
+            abi.encodeWithSignature("setX(uint256)", x)
+        );
+
+        emit Response(success, data); //释放事件
+    }
+
+    function callGetX(address _addr) external returns(uint256){
+        // call getX()
+        (bool success, bytes memory data) = _addr.call(
+            abi.encodeWithSignature("getX()")
+        );
+
+        emit Response(success, data); //释放事件
+        return abi.decode(data, (uint256));
+    }
+
+    function callNonExist(address _addr) external{
+        // call getX()
+        (bool success, bytes memory data) = _addr.call(
+            abi.encodeWithSignature("foo(uint256)")
+        );
+
+        emit Response(success, data); //释放事件
+    }
+}
