1e1aa618c36bb5236c82238e7b666fffa18782c0
0xAA <d0u.gie@hotmail.com>
2022-06-18 21:06:22 +0800

    Update InsertionSort.sol

--- 07_InsertionSort/InsertionSort.sol
+++ 07_InsertionSort/InsertionSort.sol
@@ -1,75 +1,80 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-
 contract InsertionSort {
     // if else
-    function IfElseTest(uint256 _number) public returns (uint256) {
-        //if(条件){
-        //    ...;
-        //}else{
-        //    ...;
-        //}
+    function ifElseTest(uint256 _number) public pure returns(bool){
+        if(_number == 0){
+            return(true);
+        }else{
+            return(false);
+        }
     }
 
     // for loop
-    function ForLoopTest(uint256 i) public pure {
-        uint256 n = 1;
-        for (i = 0; i < n; i++) {
-            //    ...;
+    function forLoopTest() public pure returns(uint256){
+        uint sum = 0;
+        for(uint i = 0; i < 10; i++){
+            sum += i;
         }
+        return(sum);
     }
 
     // while
-    function WhileTest(uint256 i) public pure {
-        uint256 n = 0;
-        while (i < n) {
-            //   ...;
+    function whileTest() public pure returns(uint256){
+        uint sum = 0;
+        uint i = 0;
+        while(i < 10){
+            sum += i;
+            i++;
         }
+        return(sum);
     }
 
     // do-while
-    function DoWhileTest(uint256 i) public pure {
-        uint256 n = 0;
-        do {
-            // ...;
-        } while (i < n);
+    function doWhileTest() public pure returns(uint256){
+        uint sum = 0;
+        uint i = 0;
+        do{
+            sum += i;
+            i++;
+        }while(i < 10);
+        return(sum);
     }
 
+    // 三元运算符 ternary/conditional operator
+    function ternaryTest(uint256 x, uint256 y) public pure returns(uint256){
+        // return the max of x and y
+        return x >= y ? x: y; 
+    }
+
+
     // 插入排序 错误版
-    function insertionSortWrong(uint256[] memory a)
-        public
-        pure
-        returns (uint256[] memory)
-    {
+    function insertionSortWrong(uint[] memory a) public pure returns(uint[] memory) {
         // note that uint can not take negative value
-        for (uint256 i = 1; i < a.length; i++) {
-            uint256 temp = a[i];
-            uint256 j = i - 1;
-            while ((j >= 0) && (temp < a[j])) {
-                a[j + 1] = a[j];
+        for (uint i = 1;i < a.length;i++){
+            uint temp = a[i];
+            uint j=i-1;
+            while( (j >= 0) && (temp < a[j])){
+                a[j+1] = a[j];
                 j--;
             }
-            a[j + 1] = temp;
+            a[j+1] = temp;
         }
-        return (a);
+        return(a);
     }
 
     // 插入排序 正确版
-    function insertionSort(uint256[] memory a)
-        public
-        pure
-        returns (uint256[] memory)
-    {
+    function insertionSort(uint[] memory a) public pure returns(uint[] memory) {
         // note that uint can not take negative value
-        for (uint256 i = 1; i < a.length; i++) {
-            uint256 temp = a[i];
-            uint256 j = i;
-            while ((j >= 1) && (temp < a[j - 1])) {
-                a[j] = a[j - 1];
+        for (uint i = 1;i < a.length;i++){
+            uint temp = a[i];
+            uint j=i;
+            while( (j >= 1) && (temp < a[j-1])){
+                a[j] = a[j-1];
                 j--;
             }
             a[j] = temp;
         }
-        return (a);
+        return(a);
     }
 }
