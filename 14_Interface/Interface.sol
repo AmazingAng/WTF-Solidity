13745ecd092fcb4651dc94711ba1aa8ba6c192bd
Li, Amazing Ang <d0u.gie@hotmail.com>
2022-07-02 19:56:09 +0800

    update 14 Interface
    add more contents on interface, and illustrate how to use IERC721 to interact with BAYC contract

--- 14_Interface/Interface.sol
+++ 14_Interface/Interface.sol
@@ -28,5 +28,20 @@ interface IERC721 is IERC165 {
 
     function isApprovedForAll(address owner, address operator) external view returns (bool);
 
-    function safeTransferFrom( address from, address to, uint256 tokenId, bytes calldata data) external;
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
+}
+
+contract interactBAYC {
+    // 利用BAYC地址创建接口合约变量（ETH主网）
+    IERC721 BAYC = IERC721(0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D);
+
+    // 通过接口调用BAYC的balanceOf()查询持仓量
+    function balanceOfBAYC(address owner) external view returns (uint256 balance){
+        return BAYC.balanceOf(owner);
+    }
+
+    // 通过接口调用BAYC的safeTransferFrom()安全转账
+    function safeTransferFromBAYC(address from, address to, uint256 tokenId) external{
+        BAYC.safeTransferFrom(from, to, tokenId);
+    }
 }
