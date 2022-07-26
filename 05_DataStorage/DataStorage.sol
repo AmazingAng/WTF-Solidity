128e2a753172a4889ec01fd8fea81263d87cc5f5
Li, Amazing Ang <d0u.gie@hotmail.com>
2022-07-26 22:55:02 +0800

    Update DataStorage.sol

--- 05_DataStorage/DataStorage.sol
+++ 05_DataStorage/DataStorage.sol
@@ -27,15 +27,34 @@ contract DataStorage {
         // _x[0] = 0 //这样修改会报错
         return(_x);
     }
+}
+
+contract Variables {
+    uint public x = 1;
+    uint public y;
+    string public z;
+
+    function foo() external{
+        // 可以在函数里更改状态变量的值
+        x = 5;
+        y = 2;
+        z = "0xAA";
+    }
+
+    function bar() external pure returns(uint){
+        uint xx = 1;
+        uint yy = 3;
+        uint zz = xx + yy;
+        return(zz);
+    }
+
     function global() external view returns(address, uint, bytes memory){
         address sender = msg.sender;
         uint blockNum = block.number;
         bytes memory data = msg.data;
-        return(sender,blockNum,data);
+        return(sender, blockNum, data);
     }
 }
-
-
 
 
 
