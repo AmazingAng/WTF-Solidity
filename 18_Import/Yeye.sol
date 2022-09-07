f5292ce8f7d7d73891fb93ce1f8fc707c28b9ebe
Icarus9913 <icaruswu66@qq.com>
2022-09-07 21:30:21 +0800

    fix comments
    fix comments for 'import' chapter

--- 18_Import/Yeye.sol
+++ 18_Import/Yeye.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.4;
 contract Yeye {
     event Log(string msg);
 
-    // 定义3个function: hip(), pop(), man()，Log值为Yeye。
+    // 定义3个function: hip(), pop(), yeye()，Log值为Yeye。
     function hip() public virtual{
         emit Log("Yeye");
     }
