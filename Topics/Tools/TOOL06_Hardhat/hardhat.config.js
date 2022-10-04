6eb30b9bef47c135ea0b52df206eabb5aa155efc
chris <chris@afox.cc>
2022-10-04 11:09:00 +0800

    Added custom network

--- Topics/Tools/TOOL06_Hardhat/hardhat.config.js
+++ Topics/Tools/TOOL06_Hardhat/hardhat.config.js
@@ -2,4 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/API_KEY",
+      accounts: ["your-private-key"],
+    },
+  },
 };
