5a714f52bf604dfaf4046996074656b0ae8e21a4
Rulesbreaker <lizhangchao0517@gmail.com>
2023-01-04 22:37:50 +0800

    Update SelectorClash.sol
    make the code consistent with the code in S02_SelectorClash/readme.md

--- S02_SelectorClash/SelectorClash.sol
+++ S02_SelectorClash/SelectorClash.sol
@@ -5,13 +5,15 @@ pragma solidity ^0.8.4;
 contract SelectorClash {
     bool public solved; // 攻击是否成功
 
+    // 攻击者需要调用这个函数，但是调用者 msg.sender 必须是本合约。
     function putCurEpochConPubKeyBytes(bytes memory _bytes) public {
         require(msg.sender == address(this), "Not Owner");
         solved = true;
     }
 
-    function executeCrossChainTx(bytes memory _method, bytes memory _bytes) public returns(bool success){
-        (success, ) = address(this).call(abi.encodePacked(bytes4(keccak256(abi.encodePacked(_method, "(bytes,bytes,uint64)"))), abi.encode(_bytes)));
+    // 有漏洞，攻击者可以通过改变 _method 变量碰撞函数选择器，调用目标函数并完成攻击。
+    function executeCrossChainTx(bytes memory _method, bytes memory _bytes, bytes memory _bytes1, uint64 _num) public returns(bool success){
+        (success, ) = address(this).call(abi.encodePacked(bytes4(keccak256(abi.encodePacked(_method, "(bytes,bytes,uint64)"))), abi.encode(_bytes, _bytes1, _num)));
     }
 
     function secretSlector() external pure returns(bytes4){
