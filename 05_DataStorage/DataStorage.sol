319197de478dcdb099b235d21de7df0502ba414d
Wade <tony91097@gmail.com>
2023-01-30 15:54:31 +0800

    add 以太單位與時間單位

--- 05_DataStorage/DataStorage.sol
+++ 05_DataStorage/DataStorage.sol
@@ -54,6 +54,53 @@ contract Variables {
         bytes memory data = msg.data;
         return(sender, blockNum, data);
     }
+
+    function weiUnit() external pure returns(uint) {
+        assert(1 wei == 1e0);
+        assert(1 wei == 1);
+        return 1 wei;
+    }
+
+    function gweiUnit() external pure returns(uint) {
+        assert(1 gwei == 1e9);
+        assert(1 gwei == 1000000000);
+        return 1 gwei;
+    }
+
+    function etherUnit() external pure returns(uint) {
+        assert(1 ether == 1e18);
+        assert(1 ether == 1000000000000000000);
+        return 1 ether;
+    }
+    
+    function secondsUnit() external pure returns(uint) {
+        assert(1 seconds == 1);
+        return 1 seconds;
+    }
+
+    function minutesUnit() external pure returns(uint) {
+        assert(1 minutes == 60);
+        assert(1 minutes == 60 seconds);
+        return 1 minutes;
+    }
+
+    function hoursUnit() external pure returns(uint) {
+        assert(1 hours == 3600);
+        assert(1 hours == 60 minutes);
+        return 1 hours;
+    }
+
+    function daysUnit() external pure returns(uint) {
+        assert(1 days == 86400);
+        assert(1 days == 24 hours);
+        return 1 days;
+    }
+
+    function weeksUnit() external pure returns(uint) {
+        assert(1 weeks == 604800);
+        assert(1 weeks == 7 days);
+        return 1 weeks;
+    }
 }
 
 
