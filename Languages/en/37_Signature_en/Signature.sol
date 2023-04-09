b75d0493cbc24de31bd3c1445dd459a9ebccca9d
yeecai <yushunchn@gmail.com>
2023-04-09 19:37:52 +0800

    feat: udpate en/37

--- Languages/en/37_Signature_en/Signature.sol
+++ Languages/en/37_Signature_en/Signature.sol
@@ -6,47 +6,47 @@ import "../34_ERC721/ERC721.sol";
 // ECDSA库
 library ECDSA{
     /**
-     * @dev 通过ECDSA，验证签名地址是否正确，如果正确则返回true
-     * _msgHash为消息的hash
-     * _signature为签名
-     * _signer为签名地址
-     */
+    * @dev Verifies if the signature address is correct through ECDSA. If it is correct, returns true.
+    * _msgHash: hash of the message
+    * _signature: the signature
+    * _signer: the address that signed the message
+    */
     function verify(bytes32 _msgHash, bytes memory _signature, address _signer) internal pure returns (bool) {
         return recoverSigner(_msgHash, _signature) == _signer;
     }
 
-    // @dev 从_msgHash和签名_signature中恢复signer地址
-    function recoverSigner(bytes32 _msgHash, bytes memory _signature) internal pure returns (address){
-        // 检查签名长度，65是标准r,s,v签名的长度
+    // @dev Recovers the signer address from _msgHash and the signature _signature
+    function recoverSigner(bytes32 _msgHash, bytes memory _signature) internal pure returns (address) {
+        // Checks the length of the signature. 65 is the length of a standard r,s,v signature.
         require(_signature.length == 65, "invalid signature length");
         bytes32 r;
         bytes32 s;
         uint8 v;
-        // 目前只能用assembly (内联汇编)来从签名中获得r,s,v的值
+        // Currently, we can only use assembly to obtain the values of r,s,v from the signature.
         assembly {
             /*
-            前32 bytes存储签名的长度 (动态数组存储规则)
-            add(sig, 32) = sig的指针 + 32
-            等效为略过signature的前32 bytes
-            mload(p) 载入从内存地址p起始的接下来32 bytes数据
+            The first 32 bytes store the length of the signature (dynamic array storage rule)
+            add(sig, 32) = signature pointer + 32
+            Is equivalent to skipping the first 32 bytes of the signature
+            mload(p) loads the next 32 bytes of data from the memory address p
             */
-            // 读取长度数据后的32 bytes
+            // Reads the next 32 bytes after the length data
             r := mload(add(_signature, 0x20))
-            // 读取之后的32 bytes
+            // Reads the next 32 bytes after r
             s := mload(add(_signature, 0x40))
-            // 读取最后一个byte
+            // Reads the last byte
             v := byte(0, mload(add(_signature, 0x60)))
         }
-        // 使用ecrecover(全局函数)：利用 msgHash 和 r,s,v 恢复 signer 地址
+        // Uses ecrecover(global function) to recover the signer address from msgHash, r,s,v
         return ecrecover(_msgHash, v, r, s);
     }
-    
+
     /**
-     * @dev 返回 以太坊签名消息
-     * `hash`：消息哈希 
-     * 遵从以太坊签名标准：https://eth.wiki/json-rpc/API#eth_sign[`eth_sign`]
-     * 以及`EIP191`:https://eips.ethereum.org/EIPS/eip-191`
-     * 添加"\x19Ethereum Signed Message:\n32"字段，防止签名的是可执行交易。
+     * @dev Returns an Ethereum signed message
+     * `hash`: message hash
+     * Follows the Ethereum signing standard: https://eth.wiki/json-rpc/API#eth_sign[`eth_sign`]
+     * and `EIP191`: https://eips.ethereum.org/EIPS/eip-191`
+     * Adds the "\x19Ethereum Signed Message:\n32" field to prevent signing executable transactions.
      */
     function toEthSignedMessageHash(bytes32 hash) public pure returns (bytes32) {
         // 32 is the length in bytes of hash,
@@ -54,42 +54,44 @@ library ECDSA{
         return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
     }
 }
-
 contract SignatureNFT is ERC721 {
-    address immutable public signer; // 签名地址
-    mapping(address => bool) public mintedAddress;   // 记录已经mint的地址
+    // The address that signs the minting requests
+    address immutable public signer;
+    
+    // A mapping that tracks addresses that have already been used for minting
+    mapping(address => bool) public mintedAddress;
 
-    // 构造函数，初始化NFT合集的名称、代号、签名地址
+    // Constructor function that initializes the NFT collection's name, symbol, and signer address
     constructor(string memory _name, string memory _symbol, address _signer)
     ERC721(_name, _symbol)
     {
         signer = _signer;
     }
 
-    // 利用ECDSA验证签名并mint
+    // Validates the signature using ECDSA and then mints a new token to the specified address with the given ID
     function mint(address _account, uint256 _tokenId, bytes memory _signature)
     external
     {
-        bytes32 _msgHash = getMessageHash(_account, _tokenId); // 将_account和_tokenId打包消息
-        bytes32 _ethSignedMessageHash = ECDSA.toEthSignedMessageHash(_msgHash); // 计算以太坊签名消息
-        require(verify(_ethSignedMessageHash, _signature), "Invalid signature"); // ECDSA检验通过
-        require(!mintedAddress[_account], "Already minted!"); // 地址没有mint过
-                
-        mintedAddress[_account] = true; // 记录mint过的地址
-        _mint(_account, _tokenId); // mint
+        bytes32 _msgHash = getMessageHash(_account, _tokenId); // Concatenate the address and token ID to create a message hash
+        bytes32 _ethSignedMessageHash = ECDSA.toEthSignedMessageHash(_msgHash); // Calculate the Ethereum signed message hash
+        require(verify(_ethSignedMessageHash, _signature), "Invalid signature"); // Validate the signature using ECDSA
+        require(!mintedAddress[_account], "Already minted!"); // Make sure the address hasn't already been used for minting
+        
+        mintedAddress[_account] = true; // Record that the address has been used for minting
+        _mint(_account, _tokenId); // Mint the new token to the specified address
     }
 
     /*
-     * 将mint地址（address类型）和tokenId（uint256类型）拼成消息msgHash
+     * Concatenates the address and token ID to create a message hash
      * _account: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
      * _tokenId: 0
-     * 对应的消息msgHash: 0x1bf2c0ce4546651a1a2feb457b39d891a6b83931cc2454434f39961345ac378c
+     * The corresponding message hash: 0x1bf2c0ce4546651a1a2feb457b39d891a6b83931cc2454434f39961345ac378c
      */
     function getMessageHash(address _account, uint256 _tokenId) public pure returns(bytes32){
         return keccak256(abi.encodePacked(_account, _tokenId));
     }
 
-    // ECDSA验证，调用ECDSA库的verify()函数
+    // Validates the signature using the ECDSA library
     function verify(bytes32 _msgHash, bytes memory _signature)
     public view returns (bool)
     {
@@ -202,7 +204,7 @@ contract VerifySignature {
             uint8 v
         )
     {
-        // 检查签名长度，65是标准r,s,v签名的长度
+        // Check the length of the signature, 65 is the standard length for r, s, v signature.
         require(sig.length == 65, "invalid signature length");
 
         assembly {
