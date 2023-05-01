6a910a3a6ffa2a3002ec1f02f19234803071b65f
yeecai <yushunchn@gmail.com>
2023-05-01 16:12:01 +0800

    feat: update en/40

--- Languages/en/40_ERC1155_en/IERC1155MetadataURI.sol
+++ Languages/en/40_ERC1155_en/IERC1155MetadataURI.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.0;
 import "./IERC1155.sol";
 
 /**
- * @dev ERC1155的可选接口，加入了uri()函数查询元数据
+ * Optional interface of @dev ERC1155, added uri() function to query metadata
  */
 interface IERC1155MetadataURI is IERC1155 {
     /**
-     * @dev 返回第`id`种类代币的URI
+     * @dev Returns the URI of the `id` type token
      */
     function uri(uint256 id) external view returns (string memory);
 }
--- Languages/en/40_ERC1155_en/IERC1155Receiver.sol
+++ Languages/en/40_ERC1155_en/IERC1155Receiver.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../34_ERC721/IERC165.sol";
+import "../34_ERC721_en/IERC165.sol";
 
 /**
- * @dev ERC1155接收合约，要接受ERC1155的安全转账，需要实现这个合约
+ * @dev ERC1155 receiving contract, to accept the secure transfer of ERC1155, this contract needs to be implemented
  */
 interface IERC1155Receiver is IERC165 {
     /**
-     * @dev 接受ERC1155安全转账`safeTransferFrom` 
-     * 需要返回 0xf23a6e61 或 `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
+     * @dev accept ERC1155 safe transfer `safeTransferFrom`
+     * Need to return 0xf23a6e61 or `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
      */
     function onERC1155Received(
         address operator,
@@ -20,8 +20,8 @@ interface IERC1155Receiver is IERC165 {
     ) external returns (bytes4);
 
     /**
-     * @dev 接受ERC1155批量安全转账`safeBatchTransferFrom` 
-     * 需要返回 0xbc197c81 或 `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`
+     * @dev accept ERC1155 batch safe transfer `safeBatchTransferFrom`
+     * need to return 0xbc197c81 or `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`
      */
     function onERC1155BatchReceived(
         address operator,
