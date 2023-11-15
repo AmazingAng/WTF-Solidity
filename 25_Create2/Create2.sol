1926c5d65d94c98023db806e949b42140abca4bc
Alan Xu <xdpcsyy@gmail.com>
2023-11-15 09:48:43 +0800

    docs(25_create2): fix md's warnings and content errors
    fix md's warnings and content errors

--- 25_Create2/Create2.sol
+++ 25_Create2/Create2.sol
@@ -19,37 +19,37 @@ contract Pair{
 }
 
 contract PairFactory2{
-        mapping(address => mapping(address => address)) public getPair; // 通过两个代币地址查Pair地址
-        address[] public allPairs; // 保存所有Pair地址
+    mapping(address => mapping(address => address)) public getPair; // 通过两个代币地址查Pair地址
+    address[] public allPairs; // 保存所有Pair地址
 
-        function createPair2(address tokenA, address tokenB) external returns (address pairAddr) {
-            require(tokenA != tokenB, 'IDENTICAL_ADDRESSES'); //避免tokenA和tokenB相同产生的冲突
-            // 计算用tokenA和tokenB地址计算salt
-            (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA); //将tokenA和tokenB按大小排序
-            bytes32 salt = keccak256(abi.encodePacked(token0, token1));
-            // 用create2部署新合约
-            Pair pair = new Pair{salt: salt}(); 
-            // 调用新合约的initialize方法
-            pair.initialize(tokenA, tokenB);
-            // 更新地址map
-            pairAddr = address(pair);
-            allPairs.push(pairAddr);
-            getPair[tokenA][tokenB] = pairAddr;
-            getPair[tokenB][tokenA] = pairAddr;
-        }
+    function createPair2(address tokenA, address tokenB) external returns (address pairAddr) {
+        require(tokenA != tokenB, 'IDENTICAL_ADDRESSES'); //避免tokenA和tokenB相同产生的冲突
+        // 计算用tokenA和tokenB地址计算salt
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA); //将tokenA和tokenB按大小排序
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        // 用create2部署新合约
+        Pair pair = new Pair{salt: salt}(); 
+        // 调用新合约的initialize方法
+        pair.initialize(tokenA, tokenB);
+        // 更新地址map
+        pairAddr = address(pair);
+        allPairs.push(pairAddr);
+        getPair[tokenA][tokenB] = pairAddr;
+        getPair[tokenB][tokenA] = pairAddr;
+    }
 
-        // 提前计算pair合约地址
-        function calculateAddr(address tokenA, address tokenB) public view returns(address predictedAddress){
-            require(tokenA != tokenB, 'IDENTICAL_ADDRESSES'); //避免tokenA和tokenB相同产生的冲突
-            // 计算用tokenA和tokenB地址计算salt
-            (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA); //将tokenA和tokenB按大小排序
-            bytes32 salt = keccak256(abi.encodePacked(token0, token1));
-            // 计算合约地址方法 hash()
-            predictedAddress = address(uint160(uint(keccak256(abi.encodePacked(
-                bytes1(0xff),
-                address(this),
-                salt,
-                keccak256(type(Pair).creationCode)
-            )))));
-        }
+    // 提前计算pair合约地址
+    function calculateAddr(address tokenA, address tokenB) public view returns(address predictedAddress){
+        require(tokenA != tokenB, 'IDENTICAL_ADDRESSES'); //避免tokenA和tokenB相同产生的冲突
+        // 计算用tokenA和tokenB地址计算salt
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA); //将tokenA和tokenB按大小排序
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        // 计算合约地址方法 hash()
+        predictedAddress = address(uint160(uint(keccak256(abi.encodePacked(
+            bytes1(0xff),
+            address(this),
+            salt,
+            keccak256(type(Pair).creationCode)
+        )))));
+    }
 }
