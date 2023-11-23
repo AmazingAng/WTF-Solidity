d42034ebd6ef7de93fca503c7bb87607d1d9fb9b
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2023-11-23 02:07:24 +0100

    Update ERC20Permit.sol

--- Languages/en/53_ERC20Permit/ERC20Permit.sol
+++ Languages/en/53_ERC20Permit/ERC20Permit.sol
@@ -8,9 +8,9 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 /**
- * @dev ERC20 Permit 扩展的接口，允许通过签名进行批准，如 https://eips.ethereum.org/EIPS/eip-2612[EIP-2612]中定义。
- *
- * 添加了 {permit} 方法，可以通过帐户签名的消息更改帐户的 ERC20 余额（参见 {IERC20-allowance}）。通过不依赖 {IERC20-approve}，代币持有者的帐户无需发送交易，因此完全不需要持有 Ether。
+* @dev ERC20 Permit extended interface that allows approval via signatures, as defined in https://eips.ethereum.org/EIPS/eip-2612[EIP-2612].
+  *
+  * Added {permit} method to change an account's ERC20 balance via a message signed by the account (see {IERC20-allowance}). By not relying on {IERC20-approve}, token holders' accounts do not need to send transactions and therefore do not need to hold Ether at all.
  */
 contract ERC20Permit is ERC20, IERC20Permit, EIP712 {
     mapping(address => uint) private _nonces;
@@ -19,7 +19,7 @@ contract ERC20Permit is ERC20, IERC20Permit, EIP712 {
         keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
 
     /**
-     * @dev 初始化 EIP712 的 name 以及 ERC20 的 name 和 symbol
+     * @dev initializes the name of EIP712 and the name and symbol of ERC20
      */
     constructor(string memory name, string memory symbol) EIP712(name, "1") ERC20(name, symbol){}
 
@@ -35,18 +35,18 @@ contract ERC20Permit is ERC20, IERC20Permit, EIP712 {
         bytes32 r,
         bytes32 s
     ) public virtual override {
-        // 检查 deadline
-        require(block.timestamp <= deadline, "ERC20Permit: expired deadline");
+        // Check deadline
+         require(block.timestamp <= deadline, "ERC20Permit: expired deadline");
 
-        // 拼接 Hash
-        bytes32 structHash = keccak256(abi.encode(_PERMIT_TYPEHASH, owner, spender, value, _useNonce(owner), deadline));
-        bytes32 hash = _hashTypedDataV4(structHash);
+         // Splice Hash
+         bytes32 structHash = keccak256(abi.encode(_PERMIT_TYPEHASH, owner, spender, value, _useNonce(owner), deadline));
+         bytes32 hash = _hashTypedDataV4(structHash);
         
-        // 从签名和消息计算 signer，并验证签名
-        address signer = ECDSA.recover(hash, v, r, s);
-        require(signer == owner, "ERC20Permit: invalid signature");
+         // Calculate the signer from the signature and message, and verify the signature
+         address signer = ECDSA.recover(hash, v, r, s);
+         require(signer == owner, "ERC20Permit: invalid signature");
         
-        // 授权
+         //Authorize
         _approve(owner, spender, value);
     }
 
@@ -65,14 +65,14 @@ contract ERC20Permit is ERC20, IERC20Permit, EIP712 {
     }
 
     /**
-     * @dev "消费nonce": 返回 `owner` 当前的 `nonce`，并增加 1。
-     */
-    function _useNonce(address owner) internal virtual returns (uint256 current) {
-        current = _nonces[owner];
-        _nonces[owner] += 1;
-    }
+     * @dev "Consumption nonce": Returns the current `nonce` of the `owner` and increases it by 1.
+      */
+     function _useNonce(address owner) internal virtual returns (uint256 current) {
+         current = _nonces[owner];
+         _nonces[owner] += 1;
+     }
 
-    // @dev 铸造代币
+     // @dev mint tokens
     function mint(uint amount) external {
         _mint(msg.sender, amount);
     }
