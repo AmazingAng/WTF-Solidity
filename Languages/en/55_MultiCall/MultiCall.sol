7cc8da1fcf34fc1ba733ce1861863e5e2d65b382
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2023-11-25 00:14:44 +0100

    Update MultiCall.sol

--- Languages/en/55_MultiCall/MultiCall.sol
+++ Languages/en/55_MultiCall/MultiCall.sol
@@ -2,36 +2,36 @@
 pragma solidity ^0.8.19;
 
 contract Multicall {
-    // Call结构体，包含目标合约target，是否允许调用失败allowFailure，和call data
-    struct Call {
-        address target;
-        bool allowFailure;
-        bytes callData;
-    }
+     // Call structure, including target contract target, whether to allow call failure allowFailure, and call data
+     struct Call {
+         address target;
+         bool allowFailure;
+         bytes callData;
+     }
 
-    // Result结构体，包含调用是否成功和return data
-    struct Result {
-        bool success;
-        bytes returnData;
-    }
+     // Result structure, including whether the call is successful and return data
+     struct Result {
+         bool success;
+         bytes returnData;
+     }
 
-    /// @notice 将多个调用（支持不同合约/不同方法/不同参数）合并到一次调用
-    /// @param calls Call结构体组成的数组
-    /// @return returnData Result结构体组成的数组
-    function multicall(Call[] calldata calls) public returns (Result[] memory returnData) {
-        uint256 length = calls.length;
-        returnData = new Result[](length);
-        Call calldata calli;
+     /// @notice merges multiple calls (supporting different contracts/different methods/different parameters) into one call
+     /// @param calls Array composed of Call structure
+     /// @return returnData An array composed of Result structure
+     function multicall(Call[] calldata calls) public returns (Result[] memory returnData) {
+         uint256 length = calls.length;
+         returnData = new Result[](length);
+         Call calldata calli;
         
-        // 在循环中依次调用
-        for (uint256 i = 0; i < length; i++) {
-            Result memory result = returnData[i];
-            calli = calls[i];
-            (result.success, result.returnData) = calli.target.call(calli.callData);
-            // 如果 calli.allowFailure 和 result.success 均为 false，则 revert
-            if (!(calli.allowFailure || result.success)){
-                revert("Multicall: call failed");
-            }
-        }
-    }
+         // Called sequentially in the loop
+         for (uint256 i = 0; i < length; i++) {
+             Result memory result = returnData[i];
+             calli = calls[i];
+             (result.success, result.returnData) = calli.target.call(calli.callData);
+             // If calli.allowFailure and result.success are both false, revert
+             if (!(calli.allowFailure || result.success)){
+                 revert("Multicall: call failed");
+             }
+         }
+     }
 }
