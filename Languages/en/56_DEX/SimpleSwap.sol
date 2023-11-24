3b74a0ef6e27129aee65c5b909b8c9749c7186a3
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2023-11-25 00:33:57 +0100

    Update SimpleSwap.sol

--- Languages/en/56_DEX/SimpleSwap.sol
+++ Languages/en/56_DEX/SimpleSwap.sol
@@ -4,151 +4,151 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SimpleSwap is ERC20 {
-    // 代币合约
-    IERC20 public token0;
-    IERC20 public token1;
+     //Token contract
+     IERC20 public token0;
+     IERC20 public token1;
 
-    // 代币储备量
-    uint public reserve0;
-    uint public reserve1;
+     //Token reserve amount
+     uint public reserve0;
+     uint public reserve1;
     
-    // 事件 
-    event Mint(address indexed sender, uint amount0, uint amount1);
-    event Burn(address indexed sender, uint amount0, uint amount1);
-    event Swap(
-        address indexed sender,
-        uint amountIn,
-        address tokenIn,
-        uint amountOut,
-        address tokenOut
-        );
+     // event 
+     event Mint(address indexed sender, uint amount0, uint amount1);
+     event Burn(address indexed sender, uint amount0, uint amount1);
+     eventSwap(
+         address indexed sender,
+         uint amountIn,
+         address tokenIn,
+         uint amountOut,
+         address tokenOut
+         );
 
-    // 构造器，初始化代币地址
-    constructor(IERC20 _token0, IERC20 _token1) ERC20("SimpleSwap", "SS") {
-        token0 = _token0;
-        token1 = _token1;
-    }
+     //Constructor, initialize token address
+     constructor(IERC20 _token0, IERC20 _token1) ERC20("SimpleSwap", "SS") {
+         token0 = _token0;
+         token1 = _token1;
+     }
 
-    // 取两个数的最小值
-    function min(uint x, uint y) internal pure returns (uint z) {
-        z = x < y ? x : y;
-    }
+     // Get the minimum of two numbers
+     function min(uint x, uint y) internal pure returns (uint z) {
+         z = x < y ? x : y;
+     }
 
-    // 计算平方根 babylonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
-    function sqrt(uint y) internal pure returns (uint z) {
-        if (y > 3) {
-            z = y;
-            uint x = y / 2 + 1;
-            while (x < z) {
-                z = x;
-                x = (y / x + x) / 2;
-            }
-        } else if (y != 0) {
-            z = 1;
+     // Compute square roots babylonian method (https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method)
+     function sqrt(uint y) internal pure returns (uint z) {
+         if (y > 3) {
+             z = y;
+             uint x = y / 2 + 1;
+             while (x < z) {
+                 z = x;
+                 x = (y / x + x) / 2;
+             }
+         } else if (y != 0) {
+             z = 1;
         }
     }
 
-    // 添加流动性，转进代币，铸造LP
-    // 如果首次添加，铸造的LP数量 = sqrt(amount0 * amount1)
-    // 如果非首次，铸造的LP数量 = min(amount0/reserve0, amount1/reserve1)* totalSupply_LP
-    // @param amount0Desired 添加的token0数量
-    // @param amount1Desired 添加的token1数量
-    function addLiquidity(uint amount0Desired, uint amount1Desired) public returns(uint liquidity){
-        // 将添加的流动性转入Swap合约，需事先给Swap合约授权
-        token0.transferFrom(msg.sender, address(this), amount0Desired);
-        token1.transferFrom(msg.sender, address(this), amount1Desired);
-        // 计算添加的流动性
-        uint _totalSupply = totalSupply();
-        if (_totalSupply == 0) {
-            // 如果是第一次添加流动性，铸造 L = sqrt(x * y) 单位的LP（流动性提供者）代币
-            liquidity = sqrt(amount0Desired * amount1Desired);
-        } else {
-            // 如果不是第一次添加流动性，按添加代币的数量比例铸造LP，取两个代币更小的那个比例
-            liquidity = min(amount0Desired * _totalSupply / reserve0, amount1Desired * _totalSupply /reserve1);
-        }
+// Add liquidity, transfer tokens, and mint LP
+     // If added for the first time, the amount of LP minted = sqrt(amount0 * amount1)
+     // If it is not the first time, the amount of LP minted = min(amount0/reserve0, amount1/reserve1)* totalSupply_LP
+     // @param amount0Desired The amount of token0 added
+     // @param amount1Desired The amount of token1 added
+     function addLiquidity(uint amount0Desired, uint amount1Desired) public returns(uint liquidity){
+         // To transfer the added liquidity to the Swap contract, you need to give the Swap contract authorization in advance.
+         token0.transferFrom(msg.sender, address(this), amount0Desired);
+         token1.transferFrom(msg.sender, address(this), amount1Desired);
+         // Calculate added liquidity
+         uint _totalSupply = totalSupply();
+         if (_totalSupply == 0) {
+             // If liquidity is added for the first time, mint L = sqrt(x * y) units of LP (liquidity provider) tokens
+             liquidity = sqrt(amount0Desired * amount1Desired);
+         } else {
+             // If it is not the first time to add liquidity, LP will be minted in proportion to the number of added tokens, and the smaller ratio of the two tokens will be used.
+             liquidity = min(amount0Desired * _totalSupply / reserve0, amount1Desired * _totalSupply /reserve1);
+         }
 
-        // 检查铸造的LP数量
-        require(liquidity > 0, 'INSUFFICIENT_LIQUIDITY_MINTED');
+// Check the amount of LP minted
+         require(liquidity > 0, 'INSUFFICIENT_LIQUIDITY_MINTED');
 
-        // 更新储备量
-        reserve0 = token0.balanceOf(address(this));
-        reserve1 = token1.balanceOf(address(this));
+         // Update reserve
+         reserve0 = token0.balanceOf(address(this));
+         reserve1 = token1.balanceOf(address(this));
 
-        // 给流动性提供者铸造LP代币，代表他们提供的流动性
-        _mint(msg.sender, liquidity);
+         // Mint LP tokens for liquidity providers to represent the liquidity they provide
+         _mint(msg.sender, liquidity);
         
-        emit Mint(msg.sender, amount0Desired, amount1Desired);
+         emit Mint(msg.sender, amount0Desired, amount1Desired);
+     }
+
+// Remove liquidity, destroy LP, and transfer tokens
+     // Transfer quantity = (liquidity / totalSupply_LP) * reserve
+     // @param liquidity The amount of liquidity removed
+     function removeLiquidity(uint liquidity) external returns (uint amount0, uint amount1) {
+         // Get balance
+         uint balance0 = token0.balanceOf(address(this));
+         uint balance1 = token1.balanceOf(address(this));
+         // Calculate the number of tokens to be transferred according to the proportion of LP
+         uint _totalSupply = totalSupply();
+         amount0 = liquidity * balance0 / _totalSupply;
+         amount1 = liquidity * balance1 / _totalSupply;
+         // Check the number of tokens
+         require(amount0 > 0 && amount1 > 0, 'INSUFFICIENT_LIQUIDITY_BURNED');
+         // Destroy LP
+         _burn(msg.sender, liquidity);
+         // Transfer tokens
+         token0.transfer(msg.sender, amount0);
+         token1.transfer(msg.sender, amount1);
+         // Update reserve
+         reserve0 = token0.balanceOf(address(this));
+         reserve1 = token1.balanceOf(address(this));
+
+         emit Burn(msg.sender, amount0, amount1);
     }
 
-    // 移除流动性，销毁LP，转出代币
-    // 转出数量 = (liquidity / totalSupply_LP) * reserve
-    // @param liquidity 移除的流动性数量
-    function removeLiquidity(uint liquidity) external returns (uint amount0, uint amount1) {
-        // 获取余额
+// Given the amount of an asset and the reserve of a token pair, calculate the amount to exchange for another token
+     // Since the product is constant
+     // Before swapping: k = x * y
+     // After swapping: k = (x + delta_x) * (y + delta_y)
+     // Available delta_y = - delta_x * y / (x + delta_x)
+     // Positive/negative signs represent transfer in/out
+     function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) public pure returns (uint amountOut) {
+         require(amountIn > 0, 'INSUFFICIENT_AMOUNT');
+         require(reserveIn > 0 && reserveOut > 0, 'INSUFFICIENT_LIQUIDITY');
+         amountOut = amountIn * reserveOut / (reserveIn + amountIn);
+     }
+
+     // swap tokens
+     // @param amountIn the number of tokens used for exchange
+     // @param tokenIn token contract address used for exchange
+     // @param amountOutMin the minimum amount to exchange for another token
+     function swap(uint amountIn, IERC20 tokenIn, uint amountOutMin) external returns (uint amountOut, IERC20 tokenOut){
+         require(amountIn > 0, 'INSUFFICIENT_OUTPUT_AMOUNT');
+         require(tokenIn == token0 || tokenIn == token1, 'INVALID_TOKEN');
+        
         uint balance0 = token0.balanceOf(address(this));
         uint balance1 = token1.balanceOf(address(this));
-        // 按LP的比例计算要转出的代币数量
-        uint _totalSupply = totalSupply();
-        amount0 = liquidity * balance0 / _totalSupply;
-        amount1 = liquidity * balance1 / _totalSupply;
-        // 检查代币数量
-        require(amount0 > 0 && amount1 > 0, 'INSUFFICIENT_LIQUIDITY_BURNED');
-        // 销毁LP
-        _burn(msg.sender, liquidity);
-        // 转出代币
-        token0.transfer(msg.sender, amount0);
-        token1.transfer(msg.sender, amount1);
-        // 更新储备量
-        reserve0 = token0.balanceOf(address(this));
-        reserve1 = token1.balanceOf(address(this));
 
-        emit Burn(msg.sender, amount0, amount1);
-    }
-
-    // 给定一个资产的数量和代币对的储备，计算交换另一个代币的数量
-    // 由于乘积恒定
-    // 交换前: k = x * y
-    // 交换后: k = (x + delta_x) * (y + delta_y)
-    // 可得 delta_y = - delta_x * y / (x + delta_x)
-    // 正/负号代表转入/转出
-    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) public pure returns (uint amountOut) {
-        require(amountIn > 0, 'INSUFFICIENT_AMOUNT');
-        require(reserveIn > 0 && reserveOut > 0, 'INSUFFICIENT_LIQUIDITY');
-        amountOut = amountIn * reserveOut / (reserveIn + amountIn);
-    }
-
-    // swap代币
-    // @param amountIn 用于交换的代币数量
-    // @param tokenIn 用于交换的代币合约地址
-    // @param amountOutMin 交换出另一种代币的最低数量
-    function swap(uint amountIn, IERC20 tokenIn, uint amountOutMin) external returns (uint amountOut, IERC20 tokenOut){
-        require(amountIn > 0, 'INSUFFICIENT_OUTPUT_AMOUNT');
-        require(tokenIn == token0 || tokenIn == token1, 'INVALID_TOKEN');
-        
-        uint balance0 = token0.balanceOf(address(this));
-        uint balance1 = token1.balanceOf(address(this));
-
-        if(tokenIn == token0){
-            // 如果是token0交换token1
-            tokenOut = token1;
-            // 计算能交换出的token1数量
-            amountOut = getAmountOut(amountIn, balance0, balance1);
-            require(amountOut > amountOutMin, 'INSUFFICIENT_OUTPUT_AMOUNT');
-            // 进行交换
-            tokenIn.transferFrom(msg.sender, address(this), amountIn);
-            tokenOut.transfer(msg.sender, amountOut);
-        }else{
-            // 如果是token1交换token0
-            tokenOut = token0;
-            // 计算能交换出的token1数量
-            amountOut = getAmountOut(amountIn, balance1, balance0);
-            require(amountOut > amountOutMin, 'INSUFFICIENT_OUTPUT_AMOUNT');
-            // 进行交换
-            tokenIn.transferFrom(msg.sender, address(this), amountIn);
-            tokenOut.transfer(msg.sender, amountOut);
+            if(tokenIn == token0){
+             // If token0 is exchanged for token1
+             tokenOut = token1;
+             // Calculate the number of token1 that can be exchanged
+             amountOut = getAmountOut(amountIn, balance0, balance1);
+             require(amountOut > amountOutMin, 'INSUFFICIENT_OUTPUT_AMOUNT');
+             //Exchange
+             tokenIn.transferFrom(msg.sender, address(this), amountIn);
+             tokenOut.transfer(msg.sender, amountOut);
+             }else{
+             // If token1 is exchanged for token0
+             tokenOut = token0;
+             // Calculate the number of token1 that can be exchanged
+             amountOut = getAmountOut(amountIn, balance1, balance0);
+             require(amountOut > amountOutMin, 'INSUFFICIENT_OUTPUT_AMOUNT');
+             //Exchange
+             tokenIn.transferFrom(msg.sender, address(this), amountIn);
+             tokenOut.transfer(msg.sender, amountOut);
         }
 
-        // 更新储备量
+        // Update reserve
         reserve0 = token0.balanceOf(address(this));
         reserve1 = token1.balanceOf(address(this));
 
