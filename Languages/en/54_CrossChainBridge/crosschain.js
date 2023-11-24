78b5db52d73b5a91dfddb62821d4b5f7402d0422
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2023-11-25 00:05:06 +0100

    Update crosschain.js

--- Languages/en/54_CrossChainBridge/crosschain.js
+++ Languages/en/54_CrossChainBridge/crosschain.js
@@ -1,16 +1,16 @@
 import { ethers } from "ethers";
 
-// 初始化两条链的provider
+//Initialize the providers of the two chains
 const providerGoerli = new ethers.JsonRpcProvider("Goerli_Provider_URL");
 const providerSepolia = new ethers.JsonRpcProvider("Sepolia_Provider_URL://eth-sepolia.g.alchemy.com/v2/RgxsjQdKTawszh80TpJ-14Y8tY7cx5W2");
 
-// 初始化两条链的signer
-// privateKey填管理者钱包的私钥
+//Initialize the signers of the two chains
+// privateKey fills in the private key of the administrator's wallet
 const privateKey = "Your_Key";
 const walletGoerli = new ethers.Wallet(privateKey, providerGoerli);
 const walletSepolia = new ethers.Wallet(privateKey, providerSepolia);
 
-// 合约地址和ABI
+//Contract address and ABI
 const contractAddressGoerli = "0xa2950F56e2Ca63bCdbA422c8d8EF9fC19bcF20DD";
 const contractAddressSepolia = "0xad20993E1709ed13790b321bbeb0752E50b8Ce69";
 
@@ -20,30 +20,30 @@ const abi = [
     "function mint(address to, uint amount) external",
 ];
 
-// 初始化合约实例
+//Initialize contract instance
 const contractGoerli = new ethers.Contract(contractAddressGoerli, abi, walletGoerli);
 const contractSepolia = new ethers.Contract(contractAddressSepolia, abi, walletSepolia);
 
 const main = async () => {
     try{
-        console.log(`开始监听跨链事件`)
+        console.log(`Start listening to cross-chain events`)
 
-        // 监听chain Sepolia的Bridge事件，然后在Goerli上执行mint操作，完成跨链
+       // Listen to the Bridge event of chain Sepolia, and then perform the mint operation on Goerli to complete the cross-chain
         contractSepolia.on("Bridge", async (user, amount) => {
             console.log(`Bridge event on Chain Sepolia: User ${user} burned ${amount} tokens`);
 
-            // 在执行burn操作
+            // Performing burn operation
             let tx = await contractGoerli.mint(user, amount);
             await tx.wait();
 
             console.log(`Minted ${amount} tokens to ${user} on Chain Goerli`);
         });
 
-        // 监听chain Sepolia的Bridge事件，然后在Goerli上执行mint操作，完成跨链
+        // Listen to the Bridge event of chain Sepolia, and then perform the mint operation on Goerli to complete the cross-chain
         contractGoerli.on("Bridge", async (user, amount) => {
             console.log(`Bridge event on Chain Goerli: User ${user} burned ${amount} tokens`);
 
-            // 在执行burn操作
+            // Performing burn operation
             let tx = await contractSepolia.mint(user, amount);
             await tx.wait();
 
