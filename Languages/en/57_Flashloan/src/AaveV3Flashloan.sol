95a3bae8a53e7b18166bdda551381693be046abf
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2023-11-25 02:32:36 +0100

    Update AaveV3Flashloan.sol

--- Languages/en/57_Flashloan/src/AaveV3Flashloan.sol
+++ Languages/en/57_Flashloan/src/AaveV3Flashloan.sol
@@ -4,64 +4,64 @@ pragma solidity ^0.8.20;
 import "./Lib.sol";
 
 interface IFlashLoanSimpleReceiver {
-    /**
-    * @notice 在接收闪电借款资产后执行操作
-    * @dev 确保合约能够归还债务 + 额外费用，例如，具有
-    *      足够的资金来偿还，并已批准 Pool 提取总金额
-    * @param asset 闪电借款资产的地址
-    * @param amount 闪电借款资产的数量
-    * @param premium 闪电借款资产的费用
-    * @param initiator 发起闪电贷款的地址
-    * @param params 初始化闪电贷款时传递的字节编码参数
-    * @return 如果操作的执行成功则返回 True，否则返回 False
-    */
-    function executeOperation(
-        address asset,
-        uint256 amount,
-        uint256 premium,
-        address initiator,
-        bytes calldata params
-    ) external returns (bool);
+     /**
+     * @notice performs operations after receiving flash loan assets
+     * @dev ensures that the contract can pay off the debt + additional fees, e.g. with
+     * Sufficient funds to repay and Pool has been approved to withdraw the total amount
+     * @param asset The address of the flash loan asset
+     * @param amount The amount of flash loan assets
+     * @param premium The fee for lightning borrowing assets
+     * @param initiator The address where flash loans are initiated
+     * @param params byte encoding parameters passed when initializing flash loan
+     * @return True if the operation is executed successfully, False otherwise
+     */
+     function executeOperation(
+         address asset,
+         uint256 amount,
+         uint256 premium,
+         address initiator,
+         bytes calldata params
+     ) external returns (bool);
 }
 
-// AAVE V3闪电贷合约
+// AAVE V3 flash loan contract
 contract AaveV3Flashloan {
-    address private constant AAVE_V3_POOL =
-        0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2;
+     address private constant AAVE_V3_POOL =
+         0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2;
 
-    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-    ILendingPool public aave;
+     ILendingPool public aave;
 
-    constructor() {
-        aave = ILendingPool(AAVE_V3_POOL);
-    }
+     constructor() {
+         aave = ILendingPool(AAVE_V3_POOL);
+     }
 
-    // 闪电贷函数
-    function flashloan(uint256 wethAmount) external {
-        aave.flashLoanSimple(address(this), WETH, wethAmount, "", 0);
-    }
+     // Flash loan function
+     function flashloan(uint256 wethAmount) external {
+         aave.flashLoanSimple(address(this), WETH, wethAmount, "", 0);
+     }
 
-    // 闪电贷回调函数，只能被 pool 合约调用
-    function executeOperation(address asset, uint256 amount, uint256 premium, address initiator, bytes calldata)
-        external
-        returns (bool)
-    {   
-        // 确认调用的是 DAI/WETH pair 合约
-        require(msg.sender == AAVE_V3_POOL, "not authorized");
-        // 确认闪电贷发起者是本合约
-        require(initiator == address(this), "invalid initiator");
+     // Flash loan callback function can only be called by the pool contract
+     function executeOperation(address asset, uint256 amount, uint256 premium, address initiator, bytes calldata)
+         external
+         returns (bool)
+     {
+         // Confirm that the call is DAI/WETH pair contract
+         require(msg.sender == AAVE_V3_POOL, "not authorized");
+         // Confirm that the initiator of the flash loan is this contract
+         require(initiator == address(this), "invalid initiator");
 
-        // flashloan 逻辑，这里省略
+         // flashloan logic, omitted here
 
-        // 计算flashloan费用
-        // fee = 5/1000 * amount
-        uint fee = (amount * 5) / 10000 + 1;
-        uint amountToRepay = amount + fee;
+         // Calculate flashloan fees
+         // fee = 5/1000 * amount
+         uint fee = (amount * 5) / 10000 + 1;
+         uint amountToRepay = amount + fee;
 
-        // 归还闪电贷
-        IERC20(WETH).approve(AAVE_V3_POOL, amountToRepay);
+         //Repay flash loan
+         IERC20(WETH).approve(AAVE_V3_POOL, amountToRepay);
 
-        return true;
-    }
+         return true;
+     }
 }
