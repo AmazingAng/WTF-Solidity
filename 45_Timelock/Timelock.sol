5912171b6182982156273f503fc67c59610daf21
cxp-13 <84974164+cxp-13@users.noreply.github.com>
2023-11-25 11:31:43 +0800

    补充使用encodeWithSignature编码的注意事项

--- 45_Timelock/Timelock.sol
+++ 45_Timelock/Timelock.sol
@@ -109,6 +109,7 @@ contract Timelock{
         if (bytes(signature).length == 0) {
             callData = data;
         } else {
+// 这里如果采用encodeWithSignature的编码方式来实现调用管理员的函数，请将参数data的类型改为address。不然会导致管理员的值变为类似"0x0000000000000000000000000000000000000020"的值。其中的0x20是代表字节数组长度的意思.
             callData = abi.encodePacked(bytes4(keccak256(bytes(signature))), data);
         }
         // 利用call执行交易
