a68612899e52ac67de7e4f2961b324d5c8e3406e
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2023-11-25 02:31:08 +0100

    Update UniswapV2Flashloan.sol

--- Languages/en/57_Flashloan/src/UniswapV2Flashloan.sol
+++ Languages/en/57_Flashloan/src/UniswapV2Flashloan.sol
@@ -3,63 +3,63 @@ pragma solidity ^0.8.20;
 
 import "./Lib.sol";
 
-// UniswapV2闪电贷回调接口
+// UniswapV2 flash loan callback interface
 interface IUniswapV2Callee {
-    function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external;
+     function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external;
 }
 
-// UniswapV2闪电贷合约
+// UniswapV2 flash loan contract
 contract UniswapV2Flashloan is IUniswapV2Callee {
-    address private constant UNISWAP_V2_FACTORY =
-        0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+     address private constant UNISWAP_V2_FACTORY =
+         0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
 
-    address private constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+     address private constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-    IUniswapV2Factory private constant factory = IUniswapV2Factory(UNISWAP_V2_FACTORY);
+     IUniswapV2Factory private constant factory = IUniswapV2Factory(UNISWAP_V2_FACTORY);
 
-    IERC20 private constant weth = IERC20(WETH);
+     IERC20 private constant weth = IERC20(WETH);
 
-    IUniswapV2Pair private immutable pair;
+     IUniswapV2Pair private immutable pair;
 
-    constructor() {
-        pair = IUniswapV2Pair(factory.getPair(DAI, WETH));
-    }
+     constructor() {
+         pair = IUniswapV2Pair(factory.getPair(DAI, WETH));
+     }
 
-    // 闪电贷函数
-    function flashloan(uint wethAmount) external {
-        // calldata长度大于1才能触发闪电贷回调函数
-        bytes memory data = abi.encode(WETH, wethAmount);
+     // Flash loan function
+     function flashloan(uint wethAmount) external {
+         //The calldata length is greater than 1 to trigger the flash loan callback function
+         bytes memory data = abi.encode(WETH, wethAmount);
 
-        // amount0Out是要借的DAI, amount1Out是要借的WETH
-        pair.swap(0, wethAmount, address(this), data);
-    }
+         // amount0Out is the DAI to be borrowed, amount1Out is the WETH to be borrowed
+         pair.swap(0, wethAmount, address(this), data);
+     }
 
-    // 闪电贷回调函数，只能被 DAI/WETH pair 合约调用
-    function uniswapV2Call(
-        address sender,
-        uint amount0,
-        uint amount1,
-        bytes calldata data
-    ) external {
-        // 确认调用的是 DAI/WETH pair 合约
-        address token0 = IUniswapV2Pair(msg.sender).token0(); // 获取token0地址
-        address token1 = IUniswapV2Pair(msg.sender).token1(); // 获取token1地址
-        assert(msg.sender == factory.getPair(token0, token1)); // ensure that msg.sender is a V2 pair
+     // Flash loan callback function can only be called by the DAI/WETH pair contract
+     function uniswapV2Call(
+         address sender,
+         uint amount0,
+         uint amount1,
+         bytes calldata data
+     ) external {
+         // Confirm that the call is DAI/WETH pair contract
+         address token0 = IUniswapV2Pair(msg.sender).token0(); // Get token0 address
+         address token1 = IUniswapV2Pair(msg.sender).token1(); // Get token1 address
+         assert(msg.sender == factory.getPair(token0, token1)); // ensure that msg.sender is a V2 pair
 
-        // 解码calldata
-        (address tokenBorrow, uint256 wethAmount) = abi.decode(data, (address, uint256));
+         //Decode calldata
+         (address tokenBorrow, uint256 wethAmount) = abi.decode(data, (address, uint256));
 
-        // flashloan 逻辑，这里省略
-        require(tokenBorrow == WETH, "token borrow != WETH");
+         // flashloan logic, omitted here
+         require(tokenBorrow == WETH, "token borrow != WETH");
 
-        // 计算flashloan费用
-        // fee / (amount + fee) = 3/1000
-        // 向上取整
-        uint fee = (amount1 * 3) / 997 + 1;
-        uint amountToRepay = amount1 + fee;
+         // Calculate flashloan fees
+         // fee / (amount + fee) = 3/1000
+         // Rounded up
+         uint fee = (amount1 * 3) / 997 + 1;
+         uint amountToRepay = amount1 + fee;
 
-        // 归还闪电贷
-        weth.transfer(address(pair), amountToRepay);
-    }
+         //Repay flash loan
+         weth.transfer(address(pair), amountToRepay);
+     }
 }
