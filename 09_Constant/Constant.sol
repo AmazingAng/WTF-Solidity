b4107c6d6a7776f586d6bf030fbaacc903f67c79
Alan Xu <xdpcsyy@gmail.com>
2024-03-02 19:34:08 +0800

    docs(09_constant): update immutable
    update immutable

--- 09_Constant/Constant.sol
+++ 09_Constant/Constant.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.21;
 contract Constant {
     // constant变量必须在声明的时候初始化，之后不能改变
     uint256 public constant CONSTANT_NUM = 10;
@@ -16,7 +16,7 @@ contract Constant {
     // 利用constructor初始化immutable变量，因此可以利用
     constructor(){
         IMMUTABLE_ADDRESS = address(this);
-        IMMUTABLE_BLOCK = block.number;
+        IMMUTABLE_NUM = 1118;
         IMMUTABLE_TEST = test();
     }
 
