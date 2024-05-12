046b2f01c85edd964163e7af20713546c7050b4f
thurendous <thurendous2@gmail.com>
2024-05-12 10:28:05 +0900

    translate the solidity files as well

--- Languages/ja/18_Import/Import.sol
+++ Languages/ja/18_Import/Import.sol
@@ -2,22 +2,23 @@
 pragma solidity ^0.8.21;
 
 // 通过文件相对位置import
-import './Yeye.sol';
+import "./Yeye.sol";
 // 通过`全局符号`导入特定的合约
-import {Yeye} from './Yeye.sol';
+import {Yeye} from "./Yeye.sol";
 // 通过网址引用
-import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol';
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
 // 引用OpenZeppelin合约
-import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Import {
-    // 成功导入Address库
+    // Addressライブラリをimportできた
     using Address for address;
-    // 声明yeye变量
+    // yeye変数を宣言
+
     Yeye yeye = new Yeye();
 
-    // 测试是否能调用yeye的函数
-    function test() external{
+    // yeyeの関数を呼び出すテスト
+    function test() external {
         yeye.hip();
     }
 }
--- Languages/ja/18_Import/Yeye.sol
+++ Languages/ja/18_Import/Yeye.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-// 第10讲合约继承中的Yeye合约
+// Lesson10の継承について使ったYeyeコントラクトをimport
 contract Yeye {
     event Log(string msg);
 
-    // 定义3个function: hip(), pop(), yeye()，Log值为Yeye。
-    function hip() public virtual{
+    // 3つのfunction: hip(), pop(), yeye()を定義し、Logの値はYeye。
+    function hip() public virtual {
         emit Log("Yeye");
     }
 
-    function pop() public virtual{
+    function pop() public virtual {
         emit Log("Yeye");
     }
 
