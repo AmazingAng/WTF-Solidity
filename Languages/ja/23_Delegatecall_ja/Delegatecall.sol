5c24d6a15549e2982bd723d7e3b24b234e0ace6f
thurendous <thurendous2@gmail.com>
2024-06-03 16:40:33 +0800

    finishing translating lesson 23 and change imgs

--- Languages/ja/23_Delegatecall_ja/Delegatecall.sol
+++ Languages/ja/23_Delegatecall_ja/Delegatecall.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-// delegatecall和call类似，都是低级函数
-// call: B call C, 上下文为 C (msg.sender = B, C中的状态变量受影响)
-// delegatecall: B delegatecall C, 上下文为B (msg.sender = A, B中的状态变量受影响)
+// delegatecallはcallと似ており、低レベル関数である
+// callの場合、BがCをcallすると、コンテキストはCとなる。（msg.sender = B, Cの中の状態変数は影響を受ける）
+// delegatecallの場合、BがCをdelegatecallすると、コンテキストはBとなる。（msg.sender = A, Bの中の状態変数が影響を受ける）
 // 注意B和C的数据存储布局必须相同！变量类型、声明的前后顺序要相同，不然会搞砸合约。
+// 注意したいのは、B, Cにあるストレージのストラクチャが同じでなければならないこと。そうでないとごちゃまぜになってしまいます。
 
-// 被调用的合约C
+// 呼び出されるコントラクトC
 contract C {
     uint public num;
     address public sender;
@@ -17,19 +18,19 @@ contract C {
     }
 }
 
-// 发起delegatecall的合约B
+// delegatecallをする側のコントラクトB
 contract B {
     uint public num;
     address public sender;
 
-    // 通过call来调用C的setVars()函数，将改变合约C里的状态变量
+    // callを通じてCコントラクトを呼び出すと、Cのストレージが影響を受ける
     function callSetVars(address _addr, uint _num) external payable{
         // call setVars()
         (bool success, bytes memory data) = _addr.call(
             abi.encodeWithSignature("setVars(uint256)", _num)
         );
     }
-    // 通过delegatecall来调用C的setVars()函数，将改变合约B里的状态变量
+    // delegatecallを通じてCのsetVars()関数を呼び出し、コントラクトBの状態変数が変更される
     function delegatecallSetVars(address _addr, uint _num) external payable{
         // delegatecall setVars()
         (bool success, bytes memory data) = _addr.delegatecall(
