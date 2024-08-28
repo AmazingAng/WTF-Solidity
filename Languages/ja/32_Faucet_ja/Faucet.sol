e69d152798b8137537dc0f4ea34b87aed0553788
thurendous <thurendous2@gmail.com>
2024-08-28 21:58:37 +0900

    translate the solidity files

--- Languages/ja/32_Faucet_ja/Faucet.sol
+++ Languages/ja/32_Faucet_ja/Faucet.sol
@@ -5,44 +5,39 @@ pragma solidity ^0.8.21;
 import "./IERC20.sol"; //import IERC20
 
 contract ERC20 is IERC20 {
-
     mapping(address => uint256) public override balanceOf;
 
     mapping(address => mapping(address => uint256)) public override allowance;
 
-    uint256 public override totalSupply;   // 代币总供给
+    uint256 public override totalSupply; // token total supply
 
-    string public name;   // 名称
-    string public symbol;  // 符号
-    
-    uint8 public decimals = 18; // 小数位数
+    string public name; // 名称
+    string public symbol; // シンボル
 
-    constructor(string memory name_, string memory symbol_){
+    uint8 public decimals = 18; // 小数点以下桁数
+
+    constructor(string memory name_, string memory symbol_) {
         name = name_;
         symbol = symbol_;
     }
 
-    // @dev 实现`transfer`函数，代币转账逻辑
-    function transfer(address recipient, uint amount) public override returns (bool) {
+    // @dev  `transfer`関数の実装、トークンのトランスファロジック
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
         balanceOf[msg.sender] -= amount;
         balanceOf[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    // @dev 实现 `approve` 函数, 代币授权逻辑
-    function approve(address spender, uint amount) public override returns (bool) {
+    // @dev `approve`関数の実装、トークンの権限委任ロジック
+    function approve(address spender, uint256 amount) public override returns (bool) {
         allowance[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    // @dev 实现`transferFrom`函数，代币授权转账逻辑
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) public override returns (bool) {
+    // @dev `transferFrom`関数の実装、トークンの委任トランスファロジック
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
         allowance[sender][msg.sender] -= amount;
         balanceOf[sender] -= amount;
         balanceOf[recipient] += amount;
@@ -50,46 +45,44 @@ contract ERC20 is IERC20 {
         return true;
     }
 
-    // @dev 铸造代币，从 `0` 地址转账给 调用者地址
-    function mint(uint amount) external {
+    // @dev トークンのミント、`0`アドレスからcallerアドレスへトークンを転送
+    function mint(uint256 amount) external {
         balanceOf[msg.sender] += amount;
         totalSupply += amount;
         emit Transfer(address(0), msg.sender, amount);
     }
 
-    // @dev 销毁代币，从 调用者地址 转账给  `0` 地址
-    function burn(uint amount) external {
+    // @dev トークンをバーンする。 callerアドレスから`0`アドレスへトークンを転送
+    function burn(uint256 amount) external {
         balanceOf[msg.sender] -= amount;
         totalSupply -= amount;
         emit Transfer(msg.sender, address(0), amount);
     }
-
 }
 
-// ERC20代币的水龙头合约
+// ERC20トークンのフォーセットコントラクト
 contract Faucet {
+    uint256 public amountAllowed = 100; // 一回のリクエストで送るトークン数量
+    address public tokenContract; // tokenコントラクトアドレス
+    mapping(address => bool) public requestedAddress; // リクエスト済みアドレスの記録
 
-    uint256 public amountAllowed = 100; // 每次领 100单位代币
-    address public tokenContract;   // token合约地址
-    mapping(address => bool) public requestedAddress;   // 记录领取过代币的地址
+    // SendTokenイベント
+    event SendToken(address indexed Receiver, uint256 indexed Amount);
 
-    // SendToken事件    
-    event SendToken(address indexed Receiver, uint256 indexed Amount); 
-
-    // 部署时设定ERC20代币合约
+    // デプロイ時にtokenコントラクトアドレスを設定
     constructor(address _tokenContract) {
         tokenContract = _tokenContract; // set token contract
     }
 
-    // 用户领取代币函数
+    // ユーザーがトークンをリクエストする関数
     function requestTokens() external {
-        require(!requestedAddress[msg.sender], "Can't Request Multiple Times!"); // 每个地址只能领一次
+        require(!requestedAddress[msg.sender], "Can't Request Multiple Times!"); // アドレスごとに一回だけリクエスト可能
         IERC20 token = IERC20(tokenContract); // 创建IERC20合约对象
-        require(token.balanceOf(address(this)) >= amountAllowed, "Faucet Empty!"); // 水龙头空了
+        require(token.balanceOf(address(this)) >= amountAllowed, "Faucet Empty!"); // フォーセットの残高が足りない
 
-        token.transfer(msg.sender, amountAllowed); // 发送token
-        requestedAddress[msg.sender] = true; // 记录领取地址 
-        
-        emit SendToken(msg.sender, amountAllowed); // 释放SendToken事件
+        token.transfer(msg.sender, amountAllowed); // tokenを送る
+        requestedAddress[msg.sender] = true; // 受け取ったアドレスを記録
+
+        emit SendToken(msg.sender, amountAllowed); // SendTokenイベントを放出
     }
 }
