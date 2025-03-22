9d2ca8ded77e3d0a4e871f6bc941dfd4ca133c04
Alan Xu <xdpcsyy@gmail.com>
2025-03-23 03:26:30 +0800

    docs(07_mapping): supplement KeyType (#867)
    Supplement KeyType

--- 07_Mapping/Mapping.sol
+++ 07_Mapping/Mapping.sol
@@ -4,7 +4,8 @@ contract Mapping {
     mapping(uint => address) public idToAddress; // id映射到地址
     mapping(address => address) public swapPair; // 币对的映射，地址到地址
     
-    // 规则1. _KeyType不能是自定义的 下面这个例子会报错
+    // 规则1. _KeyType 不能是自定义的, 下面这个例子会报错
+    // _KeyType 可以是 内置值类型，string, bytes, 合约，枚举类型
     // 我们定义一个结构体 Struct
     // struct Student{
     //    uint256 id;
